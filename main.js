5aee57a5468ce75430825dd9cccc980a304079ba
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2025-01-27 12:19:58 -0500

    Basic functionality

--- main.js
+++ main.js
@@ -1,23 +1,53 @@
 const numberPool = Array(75).fill(null).map((_, i) => i + 1);
+const calledNumbers = [];
 const bingo = "BINGO";
+let intervalSeconds = 2;
+let paused = false;
 
-const allNumbers = document.querySelector(".all-numbers");
-const calledNumbers = document.querySelector(".called-numbers");
+const allNumbersElement = document.querySelector(".all-numbers");
+const calledNumbersElement = document.querySelector(".called-numbers");
 
-function mkBigNum(n) {
-    const elem = document.createElement("span");
-    elem.textContent = n.toString();
-    elem.classList.add("big-number");
-    return elem;
-}
+// function mkBigNum(n) {
+//     const elem = document.createElement("span");
+//     elem.textContent = n.toString();
+//     elem.classList.add("big-number");
+//     return elem;
+// }
 
+
+// init game
 numberPool.forEach((num, i) => {
    let newElement = document.createElement("div");
-   newElement.classList.add("small-number");
+   newElement.classList.add("small-number", "number", `small-${num}`);
    newElement.innerHTML = `<p>${bingo[Math.floor(i / 15)]} <br> ${num}</p>`;
-   allNumbers.append(newElement);
+   allNumbersElement.append(newElement);
 });
 
+startGame();
+
+
+
+// game logic
 function tick() {
-   const rand = Math.floor(Math.random() * numberPool.length);
+   const randomIndex = Math.floor(Math.random() * numberPool.length);
+   const number = numberPool[randomIndex];
+   let spliced = numberPool.splice(randomIndex, 1);
+   document.querySelector(`.small-${number}`).classList.add("completed");
+   addCalledNumber(number);
+}
+
+function startGame() {
+   let gameLoop = setInterval(() => {
+      if (!paused && numberPool.length > 0) {
+         tick();
+      }
+   }, intervalSeconds * 1000);
+}
+
+function addCalledNumber(number) {
+   calledNumbers.push(number);
+   let numberElement = document.querySelector(`.small-${number}`).cloneNode(true);
+   numberElement.classList.remove("small-number", `.small-${number}`, "completed");
+   numberElement.classList.add("large-number", `.large-${number}`);
+   calledNumbersElement.append(numberElement);
 }
